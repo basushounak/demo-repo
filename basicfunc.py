75858efe8081554bf366dd9208c3634323fa025c
basushounak <shounak.basu99@gmail.com>
2022-08-29 22:13:41 +0530

    Added new sleep function

--- basicfunc.py
+++ basicfunc.py
@@ -3,9 +3,20 @@ print("Hello World")
 def basicfunc(s):
     if s == "y":
         print("Eat something")
+
     else:
         print("Go to sleep")
+def sleepcyc(sleep):
+    if sleep == 8:
+        print("Complete Sleep")
+    elif sleep < 8:
+        print("Take Caffeine before work")
+    else:
+        print("You Dont need naps")
+
 
 s = input("Are you hungry?(y/n) : ")
+sleep = int(input("How many hours of sleep are you on ? : "))
 basicfunc(s)
+sleepcyc(sleep)
 
