570db39f74826f8a9db85f836012b7b36e2fc1bb
basushounak <shounak.basu99@gmail.com>
2022-08-29 17:38:35 +0530

    Added the basic changes

--- basicfunc.py
+++ basicfunc.py
@@ -1,1 +1,11 @@
 print("Hello World")
+
+def basicfunc(s):
+    if s == "y":
+        print("Eat something")
+    else:
+        print("Go to sleep")
+
+s = input("Are you hungry?(y/n) : ")
+basicfunc(s)
+
